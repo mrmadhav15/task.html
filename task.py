92f7f1fd75aafac97a5a0fbbb6b32dce3386d769
mrmadhav15 <93674961+mrmadhav15@users.noreply.github.com>
2023-08-31 21:09:34 +0530

    Update task.py

--- task.py
+++ task.py
@@ -4,3 +4,12 @@ from . import views
 urlpatterns = [
     path('url/task', views.task, name='task'),
 ]
+
+from django.shortcuts import render
+
+def task(request):
+    # Your dynamic data goes here
+    context = {
+        'data': 'Your dynamic data goes here'
+    }
+    return render(request, 'task.html', context)
